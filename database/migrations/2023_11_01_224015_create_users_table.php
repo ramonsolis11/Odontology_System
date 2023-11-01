ceec49bdd63ea5ae59b971369916d8f23de838c2
Ramon Solis Nuñez <ramonsolis11@gmail.com>
2023-11-01 16:52:51 -0600

    "Initial Commit"
    Odontology System

--- database/migrations/2023_11_01_224015_create_users_table.php
+++ database/migrations/2023_11_01_224015_create_users_table.php
@@ -11,10 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('username')->unique();
+            $table->string('password');
+            $table->enum('role', ['admin', 'doctor', 'patient']);
+            $table->timestamps();
         });
     }
 
@@ -23,6 +25,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('users');
     }
 };
+
